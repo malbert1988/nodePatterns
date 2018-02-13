31f9621e5c7ab6fb79ab22a0f59a72cec64d414b
Martin Gallegos <martingallegos@Air-de-Martin.lan>
2018-02-13 15:43:56 -0700

    Second

---
+++ models/log.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var Log = function(args){
+    assert.ok(args.subject && args.entry, "Need subject, entry and user id");
+    var log = {};
+    log.subject = args.subject;
+    log.entry = args.entry;
+    log.createdAt = new Date();
+    log.userId = args.userId;
+    
+    return log;
+};
